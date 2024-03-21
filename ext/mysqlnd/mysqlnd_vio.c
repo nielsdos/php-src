c8fb57134fe63c2a1d473504c6ad1148d78e8786
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-03-21 23:59:34 +0100

    Fix crash in shutdown when using a named pipe with a persistent mysqli connection
    The code originally posted in GH-10599 triggers the bug for
    non-persistent connections, but changing the host to `p:.` reveals that
    there is also a crash bug for persistent connections.

--- ext/mysqlnd/mysqlnd_vio.c
+++ ext/mysqlnd/mysqlnd_vio.c
@@ -145,6 +145,29 @@ MYSQLND_METHOD(mysqlnd_vio, open_pipe)(MYSQLND_VIO * const vio, const MYSQLND_CS
 		SET_CLIENT_ERROR(error_info, CR_CONNECTION_ERROR, UNKNOWN_SQLSTATE, "Unknown error while connecting");
 		DBG_RETURN(NULL);
 	}
+
+	if (persistent) {
+		/* This is a similar hack as for mysqlnd_vio::open_tcp_or_unix.
+		 * The main difference here is that we have no access to the hashed key.
+		 * We can however perform a loop over the persistent resource list to find
+		 * which one corresponds to our newly allocated stream.
+		 * This loop is pretty cheap because it will normally always be the first hit
+		 * when we loop backwards over the list. */
+		Bucket *bucket;
+		/* Use a bucket loop to make deletion cheap. */
+		ZEND_HASH_MAP_REVERSE_FOREACH_BUCKET(&EG(persistent_list), bucket) {
+			zend_resource *current_res = Z_RES(bucket->val);
+			if (current_res->ptr == net_stream) {
+				dtor_func_t origin_dtor = EG(persistent_list).pDestructor;
+				EG(persistent_list).pDestructor = NULL;
+				zend_hash_del_bucket(&EG(persistent_list), bucket);
+				EG(persistent_list).pDestructor = origin_dtor;
+				pefree(current_res, 1);
+				break;
+			}
+		} ZEND_HASH_FOREACH_END();
+	}
+
 	mysqlnd_fixup_regular_list(net_stream);
 
 	DBG_RETURN(net_stream);
